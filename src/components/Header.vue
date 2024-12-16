31d0d0e555856bc24a70feb990e8c0a46d67ac46
brunofelixbessa <brunofelixbessa@gmail.com>
2024-12-16 18:58:29 -0300

    dar kmode

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -20,6 +20,22 @@
         </li>
       </ul>
       <div class="flex items-center">
+        <button @click="toggleDarkMode" class="mr-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m.386-6.364l1.591 1.591M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+        </button>
         <a
           href="https://atendimento.automas.me/"
           target="_blank"
@@ -32,4 +48,22 @@
   </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from "vue";
+
+const toggleDarkMode = () => {
+  if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("darkMode", "false");
+  } else {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("darkMode", "true");
+  }
+};
+
+onMounted(() => {
+  if (localStorage.getItem("darkMode") === "true") {
+    document.documentElement.classList.add("dark");
+  }
+});
+</script>
